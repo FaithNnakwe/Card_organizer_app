bb6bca93f4ac67dceef2604b35c2142d82c4ce08
AndresZ1129 <145486662+AndresZ1129@users.noreply.github.com>
2025-03-05 17:27:02 -0500

    Working code

--- lib/helper.dart
+++ lib/helper.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
-import 'package:sqflite/sqflite.dart';
+
 import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 // Define a class to handle the database operations
 class DatabaseHelper {
@@ -56,27 +57,66 @@ class DatabaseHelper {
   // Prepopulate Cards table with standard deck of cards
   Future<void> _insertSampleData(Database db) async {
     // Folders
-    await db.insert('Folders', {'folder_name': 'Hearts', 'timestamp': DateTime.now().millisecondsSinceEpoch});
-    await db.insert('Folders', {'folder_name': 'Spades', 'timestamp': DateTime.now().millisecondsSinceEpoch});
-    await db.insert('Folders', {'folder_name': 'Diamonds', 'timestamp': DateTime.now().millisecondsSinceEpoch});
-    await db.insert('Folders', {'folder_name': 'Clubs', 'timestamp': DateTime.now().millisecondsSinceEpoch});
+    await db.insert('Folders', {
+      'folder_name': 'Hearts',
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+    });
+    await db.insert('Folders', {
+      'folder_name': 'Spades',
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+    });
+    await db.insert('Folders', {
+      'folder_name': 'Diamonds',
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+    });
+    await db.insert('Folders', {
+      'folder_name': 'Clubs',
+      'timestamp': DateTime.now().millisecondsSinceEpoch,
+    });
 
     // Get the folder IDs
     List<Map<String, dynamic>> folderResult = await db.query('Folders');
-    int heartsFolderId = folderResult.firstWhere((folder) => folder['folder_name'] == 'Hearts')['id'];
-    int spadesFolderId = folderResult.firstWhere((folder) => folder['folder_name'] == 'Spades')['id'];
-    int diamondsFolderId = folderResult.firstWhere((folder) => folder['folder_name'] == 'Diamonds')['id'];
-    int clubsFolderId = folderResult.firstWhere((folder) => folder['folder_name'] == 'Clubs')['id'];
+    int heartsFolderId =
+        folderResult.firstWhere(
+          (folder) => folder['folder_name'] == 'Hearts',
+        )['id'];
+    int spadesFolderId =
+        folderResult.firstWhere(
+          (folder) => folder['folder_name'] == 'Spades',
+        )['id'];
+    int diamondsFolderId =
+        folderResult.firstWhere(
+          (folder) => folder['folder_name'] == 'Diamonds',
+        )['id'];
+    int clubsFolderId =
+        folderResult.firstWhere(
+          (folder) => folder['folder_name'] == 'Clubs',
+        )['id'];
 
     // Cards data (using URLs for simplicity)
     List<Map<String, dynamic>> cardsData = [];
     List<String> suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
-    List<String> ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
+    List<String> ranks = [
+      'Ace',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'Jack',
+      'Queen',
+      'King',
+    ];
 
     for (var suit in suits) {
       for (var i = 0; i < ranks.length; i++) {
         String cardName = '${ranks[i]} of $suit';
-        String imageUrl = 'assets/images/${ranks[i]}_of_$suit.png';  // Assuming images are named like 'Ace_of_Hearts.png'
+        String imageUrl =
+            'assets/images/${ranks[i]}_of_$suit.png'; // Assuming images are named like 'Ace_of_Hearts.png'
         int folderId;
 
         switch (suit) {
@@ -112,69 +152,53 @@ class DatabaseHelper {
   }
 
   // Update Folder: Update the folder's name and timestamp
-Future<void> updateFolder(int id, String newFolderName) async {
-  final db = await database;
-  
-  await db.update(
-    'Folders',
-    {
-      'folder_name': newFolderName,
-      'timestamp': DateTime.now().millisecondsSinceEpoch,
-    },
-    where: 'id = ?',
-    whereArgs: [id],
-  );
+  Future<void> updateFolder(int id, String newFolderName) async {
+    final db = await database;
+
+    await db.update(
+      'Folders',
+      {
+        'folder_name': newFolderName,
+        'timestamp': DateTime.now().millisecondsSinceEpoch,
+      },
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // Update Card: Update a card's name, suit, or image URL by card ID
+  Future<void> updateCard(
+    int id, {
+    String? newName,
+    String? newSuit,
+    String? newImageUrl,
+  }) async {
+    final db = await database;
+
+    Map<String, dynamic> updatedCard = {};
+    if (newName != null) updatedCard['name'] = newName;
+    if (newSuit != null) updatedCard['suit'] = newSuit;
+    if (newImageUrl != null) updatedCard['image_url'] = newImageUrl;
+
+    await db.update('Cards', updatedCard, where: 'id = ?', whereArgs: [id]);
+  }
+
+  // Delete Folder: Delete a folder by its ID
+  Future<void> deleteFolder(int id) async {
+    final db = await database;
+
+    // First, delete all cards in the folder to avoid foreign key constraint errors
+    await db.delete('Cards', where: 'folder_id = ?', whereArgs: [id]);
+
+    // Then delete the folder itself
+    await db.delete('Folders', where: 'id = ?', whereArgs: [id]);
+  }
+
+  // Delete Card: Delete a card by its ID
+  Future<void> deleteCard(int id) async {
+    final db = await database;
+
+    // Delete the card from the Cards table
+    await db.delete('Cards', where: 'id = ?', whereArgs: [id]);
+  }
 }
-
-// Update Card: Update a card's name, suit, or image URL by card ID
-Future<void> updateCard(int id, {String? newName, String? newSuit, String? newImageUrl}) async {
-  final db = await database;
-
-  Map<String, dynamic> updatedCard = {};
-  if (newName != null) updatedCard['name'] = newName;
-  if (newSuit != null) updatedCard['suit'] = newSuit;
-  if (newImageUrl != null) updatedCard['image_url'] = newImageUrl;
-
-  await db.update(
-    'Cards',
-    updatedCard,
-    where: 'id = ?',
-    whereArgs: [id],
-  );
-}
-
-// Delete Folder: Delete a folder by its ID
-Future<void> deleteFolder(int id) async {
-  final db = await database;
-
-  // First, delete all cards in the folder to avoid foreign key constraint errors
-  await db.delete(
-    'Cards',
-    where: 'folder_id = ?',
-    whereArgs: [id],
-  );
-
-  // Then delete the folder itself
-  await db.delete(
-    'Folders',
-    where: 'id = ?',
-    whereArgs: [id],
-  );
-}
-
-// Delete Card: Delete a card by its ID
-Future<void> deleteCard(int id) async {
-  final db = await database;
-
-  // Delete the card from the Cards table
-  await db.delete(
-    'Cards',
-    where: 'id = ?',
-    whereArgs: [id],
-  );
-}
-
-}
- 
-
- 
--- lib/main.dart
+++ lib/main.dart
@@ -1,52 +1,56 @@
 import 'package:flutter/material.dart';
 
-void main() {
+import 'helper.dart';
+
+final dbHelper = DatabaseHelper();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dbHelper.database;
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Card Folders',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Card Folders'),
     );
   }
 }
 
+class CardModel {
+  final int id;
+  final String name;
+  final String suit;
+  final String imageUrl;
+  final int folderId;
+
+  CardModel({
+    required this.id,
+    required this.name,
+    required this.suit,
+    required this.imageUrl,
+    required this.folderId,
+  });
+}
+
+class FolderModel {
+  final int id;
+  final String name;
+  final List<CardModel> cards;
+
+  FolderModel({required this.id, required this.name, required this.cards});
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -54,69 +58,99 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<FolderModel> folders = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+    _loadFoldersAndCards();
+  }
+
+  Future<void> _loadFoldersAndCards() async {
+    final db = await dbHelper.database;
+    final folderResults = await db.query('Folders');
+    final cardResults = await db.query('Cards');
+
+    List<FolderModel> loadedFolders =
+        folderResults.map((folder) {
+          List<CardModel> folderCards =
+              cardResults
+                  .where((card) => card['folder_id'] == folder['id'])
+                  .map(
+                    (card) => CardModel(
+                      id: card['id'] as int,
+                      name: card['name'] as String,
+                      suit: card['suit'] as String,
+                      imageUrl: card['image_url'] as String,
+                      folderId: card['folder_id'] as int,
+                    ),
+                  )
+                  .toList();
+
+          return FolderModel(
+            id: folder['id'] as int,
+            name: folder['folder_name'] as String,
+            cards: folderCards,
+          );
+        }).toList();
+
+    if (!mounted) return;
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      folders = loadedFolders;
     });
+  }
+
+  Future<void> _deleteCard(int cardId) async {
+    await dbHelper.deleteCard(cardId);
+    await _loadFoldersAndCards(); // Await to ensure data reloads properly
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      appBar: AppBar(title: Text(widget.title)),
+      body:
+          folders.isEmpty
+              ? const Center(child: Text('No folders available'))
+              : ListView.builder(
+                itemCount: folders.length,
+                itemBuilder: (context, index) {
+                  final folder = folders[index];
+                  return ExpansionTile(
+                    leading:
+                        folder.cards.isNotEmpty
+                            ? Image.asset(
+                              folder.cards.first.imageUrl,
+                              width: 40,
+                              height: 40,
+                              errorBuilder:
+                                  (context, error, stackTrace) =>
+                                      const Icon(Icons.error),
+                            )
+                            : const Icon(Icons.folder),
+                    title: Text(folder.name),
+                    subtitle: Text('${folder.cards.length} cards'),
+                    children:
+                        folder.cards.map((card) {
+                          return ListTile(
+                            leading: Image.asset(
+                              card.imageUrl,
+                              width: 40,
+                              height: 40,
+                              errorBuilder:
+                                  (context, error, stackTrace) =>
+                                      const Icon(Icons.error),
+                            ),
+                            title: Text(card.name),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete),
+                              onPressed: () => _deleteCard(card.id),
+                            ),
+                          );
+                        }).toList(),
+                  );
+                },
+              ),
     );
   }
 }
